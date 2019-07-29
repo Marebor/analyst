ecc38a13901aef7eae48541692da3e15f661cce8
Marek Borkowski <1marek.borkowski1@gmail.com>
2019-07-29 11:13:34 +0200

    datepicker improvement

--- src/Analyst.Web/ClientApp/src/app/date-range/date-range.component.ts
+++ src/Analyst.Web/ClientApp/src/app/date-range/date-range.component.ts
@@ -1,17 +1,30 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 import * as moment from "moment";
+import { NgbDatepicker, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-date-range',
   templateUrl: './date-range.component.html',
   styleUrls: ['./date-range.component.css']
 })
-export class DateRangeComponent {
+export class DateRangeComponent implements OnInit {
+  @ViewChild('dp') datepicker: NgbDatepicker;
   @Output() dateRangeChange: EventEmitter<{from: Date, to: Date }> = new EventEmitter<{from: Date, to: Date }>();
   hoveredDate: NgbDate;
   fromDate: Date;
   toDate: Date;
+  maxDate: NgbDateStruct;
+
+  ngOnInit() {
+    const now = new Date();
+    this.maxDate = {
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate(),
+    };
+    this.datepicker.navigateTo({ year: this.maxDate.year, month: this.maxDate.month - 1 });
+  }
 
   onDateSelection(ngbDate: NgbDate) {
     const date = this.transformDate(ngbDate);
@@ -40,6 +53,12 @@ export class DateRangeComponent {
   isRange(ngbDate: NgbDate) {
     const date = this.transformDate(ngbDate);
     return moment(date).isSame(this.fromDate) || moment(date).isSame(this.toDate) || this.isInside(ngbDate) || this.isHovered(ngbDate);
+  }
+
+  isInFuture(ngbDate: NgbDate) {
+    const now = new Date();
+    const date = this.transformDate(ngbDate);
+    return moment(date).isAfter(now);
   }
 
   private transformDate(date: NgbDate): Date {
